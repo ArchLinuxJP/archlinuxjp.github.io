0bb1b001e0e98784031f6efb56de47a4d6cf8fdc
syui <syui@syui.cf>
2019-12-16 10:49:32 +0900

    fix json

--- bower_components/jquery.terminal/js/origin.js
+++ bower_components/jquery.terminal/js/origin.js
@@ -6,7 +6,7 @@ $(function() {
     var rsvp_url = "";
     var test_help = "Press [[b;#d33682;]<Tab>]";
 
-    axios.get('https://archlinuxjp.github.io/json/link.json', { 'Content-Type': 'application/json' })
+    axios.get('https://raw.githubusercontent.com/ArchLinuxJP/archlinuxjp.github.io/master/json/link.json', { 'Content-Type': 'application/json' })
     .then(function (response) {
       archlinuxjp_link = JSON.stringify(response.data,null,"\t");
       data = JSON.parse(archlinuxjp_link);
